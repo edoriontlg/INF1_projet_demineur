7a50a0cc5da3226b99a03d714250b7a9e782dce9
Edorion <21106616+edoriontlg@users.noreply.github.com>
2020-11-01 14:20:32 +0100

    Update projet_demineur.java

--- src/projet_demineur.java
+++ src/projet_demineur.java
@@ -261,4 +261,17 @@ public class projet_demineur {
 		
 	}
 
+
+	// As taken from
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033\143");
+            }
+        } catch (IOException | InterruptedException ex) {
+        }
+    }
 }
